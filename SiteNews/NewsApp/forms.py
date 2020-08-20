d15ef74ed2d79f3e727e35082864432fe12b3a76
kharroubi <knaoufel@yahoo.fr>
2020-08-20 16:42:38 +0300

    Add existing file

---
+++ SiteNews/NewsApp/forms.py
@@ -0,0 +1,15 @@
+
+
+
+class ContactForm(forms.Form):
+    email = forms.EmailField(required=True)
+    subject = forms.CharField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+    cc_myself = forms.BooleanField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields['email'].label = "الإيميل "
+        self.fields['subject'].label = ":العنوان"
+        self.fields['message'].label ="الرسالة"
+        self.fields['cc_myself'].label = "__ إرسال نسخة لي"
